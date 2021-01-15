64e3c5415b01df26770e43499563aa75b7420b59
danainjax <danainjax@gmail.com>
2021-01-15 10:07:09 -0500

    associations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many: assignments
+    has_many :jobs
+    has_many jobs, through :assignments
+    has_many :comments
+    has_many comments, through :jobs   
 end
