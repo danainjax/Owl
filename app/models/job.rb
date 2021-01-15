4ed168a43cd813ba1a80832fa9ae7a028e8db551
danainjax <danainjax@gmail.com>
2021-01-15 16:35:07 -0500

    updated users index page

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,14 @@ class Job < ApplicationRecord
     belongs_to :assignment
     has_many :comments, through :users
     has_one :assignment, through:users
+
+
+    def new
+        @job = Job.new
+    end
+
+    def create
+        @job = Job.new
+        @job.save
+    end
 end
