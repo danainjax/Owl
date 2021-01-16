6c654673de44545fd78443f40a57dc006dbcddcf
danainjax <danainjax@gmail.com>
2021-01-16 15:43:47 -0500

    finished signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,25 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new
-        @user.save
+        # binding.pry
+        @user = User.new(user_params)
+        if @user.save
+            #log them in
+            session[:user_id] = @user.id
+            #redirect to show page
+            redirect_to @user
+        else
+            render :new
+        end
     end
 
     def show
     end
-    
-    
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 
 end
