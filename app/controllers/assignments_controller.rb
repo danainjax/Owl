91d4467cf345181a9d6af98e6fb3d43d543a7b42
danainjax <danainjax@gmail.com>
2021-01-17 10:43:50 -0500

    worked on the assignment form and added to nav bar in the making

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,21 @@
 class AssignmentsController < ApplicationController
+
+    def new
+        @assignment = Assignment.new
+    end
+
+    def create
+        @assignment = current_user.assignment.build(assignment_params)
+        if @assignment.save
+            redirect_to @assignment
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def assignment_params
+        params.require(:assignment).permit(:status)
+    end
 end
