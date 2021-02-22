ec5a7ecd81e96390bd7e3e285bc5ecfb5f19f464
danainjax <danainjax@gmail.com>
2021-02-22 09:16:14 -0500

    removed task helper method, not needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,19 @@
 class ApplicationController < ActionController::Base
     #gives access to methods in the views
-    helper_method :current_user, :logged_in?, :current_task, :redirect_if_not_logged_in, :currrent_ticket
+    helper_method :current_user, :logged_in?, :redirect_if_not_logged_in, :currrent_ticket
 
 private
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
-    def current_task
-        @current_task ||= Task.find_by_id(session[:task_id]) if session[:task_id]
-    end
 
     def current_assignment 
         @current_assignment ||= Assignment.find_by_id(session[:assignment_id]) if session[:assignment_id]
     end
 
     def current_ticket
-        @current_ticket ||= TIcket.find_by_id(session[:ticket_id]) if session[:ticket_id] 
+        @current_ticket ||= Ticket.find_by_id(session[:ticket_id]) if session[:ticket_id] 
     end
 
     def logged_in?
